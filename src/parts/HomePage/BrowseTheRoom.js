7fe98e445eee4b62456ff992ac0f527f4565047a
usman <usmanabdat6@gmail.com>
2022-09-21 16:47:26 +0700

    complete homepage

--- src/parts/HomePage/BrowseTheRoom.js
+++ src/parts/HomePage/BrowseTheRoom.js
@@ -1,80 +1,123 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import useAsync from 'helper/hooks/useAsync';
+
+import fetch from 'helper/fetch/index';
+
+function Loading({ratio = {}}) {
+  const dummy = [
+    {
+        "id": 1, 
+        "ratio": {
+            "default": "1/9",
+            "md": "1/4"
+        }
+    },
+    {
+        "id": 2,
+        "ratio": {
+            "default": "1/9",
+            "md": "2/2"
+        }
+    },
+    {
+        "id": 3, 
+        "ratio": {
+            "default": "1/9",
+            "md": "2/3"
+        }
+    },
+    {
+        "id": 4, 
+        "ratio": {
+            "default": "1/9",
+            "md": "1/4"
+        }
+    }
+];
+
+  return dummy.map((item, index) => {
+    return <div key={item.id}
+    className={`relative card ${ratio?.wrapper.default?.[item.ratio.default]} ${ratio?.wrapper.md?.[item.ratio.md]}`} style={{height: index === 0 ? 180 : "auto" }}>
+    
+    <div className="bg-gray-300 rounded-lg w-full h-full">
+      <div className={`overlay ${ratio?.meta?.[item.ratio.md]}`}>
+        <div className="w-24 h-3 bg-gray-400 mt-3 rounded-full"></div>
+        <div className="w-36 h-3 bg-gray-400 mt-2 rounded-full"></div>
+      </div>
+    </div>
+
+    <Link to="/categories/:idc" className="stretched-link">
+      {/* <!-- fake children --> */}
+    </Link>
+  </div>
+  })
+}
 
 export default function BrowseTheRoom() {
+
+  const { data, status, error, run, isLoading } = useAsync();
+
+  useEffect(() => {
+    run (
+      fetch(
+        { url : "/api/categories/?page=1&limit=4" }
+      )
+    )
+  }, [run])
+  
+  // console.log(data,status, error, run);
+  // if(isLoading) return "Loading";
+
+  const ratioClassName = {
+    wrapper: {
+      default: {
+        "1/9": "col-span-9 row-span-1",
+      },
+      md: {
+        "1/4": "md:col-span-4 md:row-span-1",
+        "2/2": "md:col-span-2 md:row-span-2",
+        "2/3": "md:col-span-3 md:row-span-2",
+      }
+    },
+    meta: {
+        "1/9": "left-0 top-0 bottom-0 flex justify-center flex-col pl-48 md:pl-72",
+        "1/4": "left-0 top-0 bottom-0 flex justify-center flex-col pl-48 md:pl-72",
+        "2/2": "inset-0 md:bottom-auto flex justify-center md:items-center flex-col pl-48 md:pl-0 pt-0 md:pt-12",
+        "2/3": "inset-0 md:bottom-auto flex justify-center md:items-center flex-col pl-48 md:pl-0 pt-0 md:pt-12",
+    }
+  }
+
+
   return (
     <section className="flex bg-gray-100 py-16 px-4" id="browse-the-room">
       <div className="container mx-auto">
         <div className="flex flex-start mb-4">
           <h3 className="text-2xl capitalize font-semibold">browse the room<br className="" />that we design for you</h3>
         </div>
+        
         <div className="grid grid-rows-2 grid-cols-9 gap-4">
 
-          <div className="relative col-span-9 row-span-1 md:col-span-4 card" style={{height: 180 }}>
+        {
+          isLoading ? <Loading ratio={ratioClassName}/> : 
+          data.data.map(( item,index ) => {
+            return <div key={item.id}
+            className={`relative card ${ratioClassName?.wrapper.default?.[item.ratio.default]} ${ratioClassName?.wrapper.md?.[item.ratio.md]}`} style={{height: index === 0 ? 180 : "auto" }}>
             <div className="card-shadow rounded-xl">
-              <img src="./images/content/image-catalog-1.jpg" alt="" className="w-full h-full object-cover overlay overflow-hidden rounded-xl" />
+              <img src={`images/content/${item.imageUrl}`} alt={item.title} className="w-full h-full object-cover overlay overflow-hidden rounded-xl" />
             </div>
-            <div className="overlay left-0 top-0 bottom-0 flex justify-center flex-col pl-48 md:pl-72">
+            <div className={`overlay ${ratioClassName?.meta?.[item.ratio.md]}`}>
               <h5 className="text-lg font-semibold">
-                Living Room
+                {item.title}
               </h5>
-              <span className="">18.309 items</span>
+              <span className="">{item.products} item{item.products > 1 ? "s" : ""}</span>
             </div>
             <Link to="/categories/:idc" className="stretched-link">
               {/* <!-- fake children --> */}
             </Link>
           </div>
-
-
-
-          <div className="relative col-span-9 row-span-1 md:col-span-2 md:row-span-2 card">
-            <div className="card-shadow rounded-xl">
-              <img src="./images/content/image-catalog-4.jpg" alt="" className="w-full h-full object-cover overlay overflow-hidden rounded-xl" />
-            </div>
-            <div className="overlay inset-0  md:bottom-auto flex justify-center md:items-center flex-col pl-48 md:pl-0 pt-0 md:pt-12">
-              <h5 className="text-lg font-semibold">
-                Decoration
-              </h5>
-              <span className="">77.392 items</span>
-            </div>
-            <Link to="/categories/:idc" className="stretched-link">
-              {/* <!-- fake children --> */}
-            </Link>
-          </div>
-
-
-
-          <div className="relative col-span-9 row-span-1 md:col-span-3 md:row-span-2 card">
-            <div className="card-shadow rounded-xl">
-              <img src="./images/content/image-catalog-3.jpg" alt="" className="w-full h-full object-cover overlay overflow-hidden rounded-xl" />
-            </div>
-            <div className="overlay inset-0  md:bottom-auto flex justify-center md:items-center flex-col pl-48 md:pl-0 pt-0 md:pt-12">
-              <h5 className="text-lg font-semibold">
-                Bed Room
-              </h5>
-              <span className="">22.094 items</span>
-            </div>
-            <Link to="/categories/:idc" className="stretched-link">
-              {/* <!-- fake children --> */}
-            </Link>
-          </div>
-
-
-
-          <div className="relative col-span-9 row-span-1 md:col-span-4 card" style={{height: 180 }}>
-            <div className="card-shadow rounded-xl">
-              <img src="./images/content/image-catalog-2.jpg" alt="" className="w-full h-full object-cover overlay overflow-hidden rounded-xl" />
-            </div>
-            <div className="overlay left-0 top-0 bottom-0 flex justify-center flex-col pl-48 md:pl-72">
-              <h5 className="text-lg font-semibold">
-                Children Room
-              </h5>
-              <span className="">1.807 items</span>
-            </div>
-            <Link to="/categories/:idc" className="stretched-link">
-              {/* <!-- fake children --> */}
-            </Link>
-          </div>
+          })
+        }
 
         </div>
       </div>
--- src/parts/HomePage/JustArrive.js
+++ src/parts/HomePage/JustArrive.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import useAsync from 'helper/hooks/useAsync';
+import Carousel from 'components/Carousel/index';
+
+import fetch from 'helper/fetch/index';
+
+function Loading(){
+  return Array(6).fill().map((_, index) => {
+    return <div className="px-4 relative card group" key={index}>
+      <div className="rounded-xl bg-gray-300 overflow-hidden card-shadow" style={{width:287, height:386}}>
+      </div>
+        <div className="w-36 h-3 bg-gray-400 mt-2 rounded-full"></div>
+        <div className="w-24 h-3 bg-gray-400 mt-3 rounded-full"></div>
+    </div>
+  })
+}
 
 export default function JustArrive() {
+  const { data, status, error, run, isLoading } = useAsync();
+
+  const refContainer = useRef(null);
+
+  useEffect(() => {
+    run (
+      fetch(
+        { url : "/api/products/?page=1&limit=10" }
+      )
+    )
+  }, [run])
+  
+  // console.log(data,status, error, run);
+
   return (
     <section className="flex flex-col py-16">
       <div className="container mx-auto mb-4">
@@ -13,87 +42,33 @@ export default function JustArrive() {
         </div>
       </div>
       <div className="overflow-x-hidden px-4" id="carousel">
-        <div className="container mx-auto"></div>
+        <div className="container mx-auto" ref={refContainer}></div>
 
-          <div className="flex -mx-4 flex-row relative">
+          
 
-            <div className="px-4 relative card">
-              <div className="rounded-xl overflow-hidden card-shadow" style={{width:287, height:386}}>
-                <img src="./images/content/img-carousel-1.jpg" alt="cangkir" className="w-full h-full object-cover object-center" />
+            {
+              isLoading ? <div className="flex -mx-4 flex-row relative" style={{paddingLeft: refContainer.current?.getBoundingClientRect?.()?.left - 16 || 0}}> <Loading /> </div> : error ? JSON.stringify(error) : data.data.length === 0 ? "No Product Found" : <Carousel refContainer={refContainer}>
+                {data.data.map((item) => {
+                return <div key={item.id} className="px-4 relative card">
+                <div className="rounded-xl overflow-hidden card-shadow" style={{width:287, height:386}}>
+                  <img src={item.imageUrl} alt={item.title} className="w-full h-full object-cover object-center" />
+                </div>
+                <h5 className="text-lg font-semibold mt-4">{item.title}</h5>
+                <span>IDR {item.price}</span>
+                <Link to={`/categories/${item.idc}/products/${item.id}`} className="stretched-link" >
+                  {/* <!-- fake children --> */}
+                </Link>
               </div>
-              <h5 className="text-lg font-semibold mt-4">Cangkir Mauttie</h5>
-              <span>IDR 89.300</span>
-              <Link to="#details.html" className="stretched-link">
-                {/* <!-- fake children --> */}
-              </Link>
-            </div>
+              })}
+              </Carousel>
+            }
            
 
           
-            <div className="px-4 relative card">
-              <div className="rounded-xl overflow-hidden card-shadow" style={{width:287, height:386}}>
-                <img src="./images/content/img-carousel-2.jpg" alt="cangkir" className="w-full h-full object-cover object-center" />
-              </div>
-              <h5 className="text-lg font-semibold mt-4">Bankyu Minimax</h5>
-              <span>IDR 67.900</span>
-              <Link to="#details.html" className="stretched-link">
-                {/* <!-- fake children --> */}
-              </Link>
-            </div>
-           
-
-           
-            <div className="px-4 relative card">
-              <div className="rounded-xl overflow-hidden card-shadow" style={{width:287, height:386}}>
-                <img src="./images/content/img-carousel-3.jpg" alt="cangkir" className="w-full h-full object-cover object-center" />
-              </div>
-              <h5 className="text-lg font-semibold mt-4">Buku Sidu Edition</h5>
-              <span>IDR 12.000.000</span>
-              <Link to="#details.html" className="stretched-link">
-                {/* <!-- fake children --> */}
-              </Link>
-            </div>
             
-
-           
-            <div className="px-4 relative card">
-              <div className="rounded-xl overflow-hidden card-shadow" style={{width:287, height:386}}>
-                <img src="./images/content/img-carousel-4.jpg" alt="cangkir" className="w-full h-full object-cover object-center" />
-              </div>
-              <h5 className="text-lg font-semibold mt-4">Watch Notes X</h5>
-              <span>IDR 7.000.000</span>
-              <Link to="#details.html" className="stretched-link">
-                {/* <!-- fake children --> */}
-              </Link>
-            </div>
-     
-
-    
-            <div className="px-4 relative card">
-              <div className="rounded-xl overflow-hidden card-shadow" style={{width:287, height:386}}>
-                <img src="./images/content/img-carousel-5.jpg" alt="cangkir" className="w-full h-full object-cover object-center" />
-              </div>
-              <h5 className="text-lg font-semibold mt-4">Racking</h5>
-              <span>IDR 159.000</span>
-              <Link to="#details.html" className="stretched-link">
-                {/* <!-- fake children --> */}
-              </Link>
-            </div>
-
-        
-            <div className="px-4 relative card">
-              <div className="rounded-xl overflow-hidden card-shadow" style={{width:287, height:386}}>
-                <img src="./images/content/img-carousel-1.jpg" alt="cangkir" className="w-full h-full object-cover object-center" />
-              </div>
-              <h5 className="text-lg font-semibold mt-4">Bottle</h5>
-              <span>IDR 159.000</span>
-              <Link to="#details.html" className="stretched-link">
-                {/* <!-- fake children --> */}
-              </Link>
-            </div>
     
 
-          </div>
+      
         
       </div>
     </section>
